f72bd28f3d28325d13e7fd7cd17dccb90ffb469e
Sakshi-Chavan13 <sakshi.chavan@fyntune.com>
2023-11-15 19:02:55 +0530

    controllers and routes added

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,13 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ReservationController;
+use App\Http\Controllers\Admin\TableController;
+use App\Http\Controllers\Admin\MenuController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -25,6 +20,10 @@ Route::get('/dashboard', function () {
 
 Route::middleware(['auth','admin']) ->name('admin.')->prefix('admin')->group(function () {
   Route::get('/', [AdminController::class, 'index'])->name('index');
+  Route::resource('/categories',CategoryController::class);
+  Route::resource('/menus',MenuController::class);
+  Route::resource('/tables',TableController::class);
+  Route::resource('/reservation',ReservationController::class);
 });
 
 Route::middleware('auth')->group(function () {
